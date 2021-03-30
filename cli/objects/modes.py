opengnsys/ogCLI

commit be84b0a15ce5d0ada0ac1613085420b8af206efb

Add utils.py
Move auxiliary code that will be used by several objects. In this case,
scope filtering and searching by id is going to be used by 'send wol'
too.

Avoid duplicating this code creating utils.py

cli/objects/modes.py
@@ -42,26 +42,6 @@ class OgModes():
 				    help='Boot mode to be set')
 		parsed_args = parser.parse_args(args)
 
-		def scope_lookup(scope_id, scope_type, d):
-			if scope_id == d.get('id') and \
-			   scope_type == d.get('type'):
-				return d
-			for scope in d['scope']:
-				lookup = scope_lookup(scope_id,
-						      scope_type,
-						      scope)
-				if lookup is not None:
-					return lookup
-			return None
-
-		def ips_in_scope(scope):
-			if 'ip' in scope:
-				return [scope['ip']]
-			ips = []
-			for child in scope['scope']:
-				ips += ips_in_scope(child)
-			return ips
-
 		r = rest.get('/scopes')
 		scopes = r.json()
 		ips = set()

cli/utils.py
@@ -1,0 +1,24 @@
+#
+# Copyright (C) 2020 Soleta Networks <info@soleta.eu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, version 3.
+#
+
+def scope_lookup_id(scope_id, scope_type, d):
+	if scope_id == d.get('id') and scope_type == d.get('type'):
+		return d
+	for scope in d['scope']:
+		lookup = scope_lookup(scope_id, scope_type, scope)
+	if lookup is not None:
+		return lookup
+	return None
+
+def ips_in_scope(scope):
+	if 'ip' in scope:
+		return [scope['ip']]
+	ips = []
+	for child in scope['scope']:
+		ips += ips_in_scope(child)
+	return ips

commit 40bd1463770929788f64ba32b892b6af3009f929

Fix utils.py
When utils.py was created, moved functions declaration was not kept for
scope_lookup by mistake, in previous commit it was named
'scope_lookup_id'.

ips_in_scope is a function that maps a list of scopes to the list of ips
contained in it, recursively. (if the scope is a room it will gather all
computers ips in that room). Add 'None' check for its scope param,
avoiding duplication of this check in several other objects using this
function.

Also import utils in 'modes' object, which was not added previously.

Fixes: be84b0a ("Add utils.py")

cli/objects/modes.py
@@ -6,6 +6,8 @@
 # Free Software Foundation, version 3.
 #
 
+from cli.utils import *
+
 import argparse
 
 class OgModes():

cli/utils.py
@@ -6,16 +6,18 @@
 # Free Software Foundation, version 3.
 #
 
-def scope_lookup_id(scope_id, scope_type, d):
+def scope_lookup(scope_id, scope_type, d):
 	if scope_id == d.get('id') and scope_type == d.get('type'):
 		return d
 	for scope in d['scope']:
 		lookup = scope_lookup(scope_id, scope_type, scope)
-	if lookup is not None:
-		return lookup
+		if lookup is not None:
+			return lookup
 	return None
 
 def ips_in_scope(scope):
+	if scope is None:
+		return []
 	if 'ip' in scope:
 		return [scope['ip']]
 	ips = []